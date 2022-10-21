e2f37de9f4f60f3bf197ea0fe5c56832e61e9679
Juan Gustavo <juangustavomonteiro@gmail.com>
2022-10-21 14:32:28 -0300

    ajuste 1

--- src/components/outros/AgentesComponent.vue
+++ src/components/outros/AgentesComponent.vue
@@ -37,7 +37,10 @@
 			</div>
 		</div>
 		<div class="estilo-component" v-show="mostrar.length >= 1 ? true : false">
-			<component :is="mostrar.toLowerCase()" class="container"></component>
+			<component
+				:is="mostrar.toLowerCase()"
+				class="container estilo-component"
+			></component>
 		</div>
 
 		<div class="subir"><a href="#vava">Subir</a></div>
